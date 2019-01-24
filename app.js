ff4c063e247c4a04ebfc1d5a1e84a780df8d9bb5
’1608407759@qq.com <1608407759@qq.com>
2019-01-24 10:20:01 +0800

    获取数据

--- app.js
+++ app.js
@@ -1,41 +1,83 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express from 'express';
+// import db from './mongodb/db.js';
+import config from 'config-lite';
+import router from './routes/index.js';
+import cookieParser from 'cookie-parser'
+import session from 'express-session';
+import connectMongo from 'connect-mongo';
+import winston from 'winston';
+import expressWinston from 'express-winston';
+import path from 'path';
+import history from 'connect-history-api-fallback';
+import chalk from 'chalk';
+// import Statistic from './middlewares/statistic'
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
+app.all('*', (req, res, next) => {
+    const {origin, Origin, referer, Referer} = req.headers;
+    const allowOrigin = origin || Origin || referer || Referer || '*';
+    res.header("Access-Control-Allow-Origin", allowOrigin);
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Credentials", true); //可以带cookies
+    res.header("X-Powered-By", 'Express');
+    if (req.method == 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(Statistic.apiRecord)
+const MongoStore = connectMongo(session);
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    name: config.session.name,
+    secret: config.session.secret,
+    resave: true,
+    saveUninitialized: false,
+    cookie: config.session.cookie,
+    store: new MongoStore({
+        url: config.url
+    })
+}));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use(expressWinston.logger({
+//     transports: [
+//         new (winston.transports.Console)({
+//           json: true,
+//           colorize: true
+//         }),
+//         new winston.transports.File({
+//           filename: 'logs/success.log'
+//         })
+//     ]
+// }));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// router(app);
+
+// app.use(expressWinston.errorLogger({
+//     transports: [
+//         new winston.transports.Console({
+//           json: true,
+//           colorize: true
+//         }),
+//         new winston.transports.File({
+//           filename: 'logs/error.log'
+//         })
+//     ]
+// }));
+router(app);
+app.get('/a', (req, res) => {
+    res.status(200);
+    res.json(1231)
+
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use(history());
+app.use(express.static(path.join(__dirname, 'public')))
+app.listen(config.port, () => {
+    console.log(
+        chalk.green(`成功监听端口：${config.port}`)
+    )
 });
-
-module.exports = app;
